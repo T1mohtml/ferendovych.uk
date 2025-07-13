ebe145793f24f4211785beef8c2d5a83d3fbd2ad
T1mohtml <timon.black.gamig@gmail.com>
2025-07-13 20:40:53 +0300

    change stuff

--- submit.php
+++ submit.php
@@ -1,10 +1,6 @@
-<html>
-    <link rel="stylesheet" href="static/css.css">
-    </html>
-
 <?php
-$conn = new mysqli("localhost", "Timo", "2262", "visitor");
-if ($conn->connect_error) die("Connection Failed");
+$conn = new mysqli("fdb1033.awardspace.net", "4659225_suggestionbox", "T1mongogo", "4659225_suggestionbox", 3306);
+if ($conn->connect_error) die("Connection Failed: " . $conn->connect_error);
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = $conn->real_escape_string($_POST['name'] ?? '');
