5171220e4e5c142c2597a08352440b21c9516ee4
T1mohtml <timon.black.gamig@gmail.com>
2025-07-17 11:32:55 +0300

    modified:   info.php

--- info.php
+++ info.php
@@ -1,1 +1,9 @@
-<?php phpinfo(); ?>
+<?php
+$allowed_ip = '100.88.59.37'; // Replace with your actual IP
+
+if ($_SERVER['REMOTE_ADDR'] === $allowed_ip) {
+    phpinfo();
+} else {
+    echo "Access denied";
+    http_response_code(403);
+}
