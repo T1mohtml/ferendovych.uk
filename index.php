c46707ee2f6f0902f4517e030f792240afd365af
T1mohtml <timon.black.gamig@gmail.com>
2025-08-05 14:37:05 +0200

    kdfjvdighfdkjghfdkjhdfjkhfjkhdfjkghjkdfghdfjkghdfkjghdfkjghdfjkghdjkghdkjghdjkghdkghdjkghjkndfkjghdjkhdjghdjkfghfejkhgejkgf

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include_once("captain_hook.php");
     <title>ferendovych.uk</title>
 
     <!-- Favicons -->
-    <link rel="icon" type="image/png" href="website.png">
+    <link rel="icon" type="image/png" href="website.ico">
     <link rel="apple-touch-icon" sizes="180x180" href="assets/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png">
