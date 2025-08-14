edba3243a48c7d43fe5a494d2f15ce9f7c04dd31
T1mohtml <timon.black.gamig@gmail.com>
2025-08-14 18:18:34 +0200

    Restore icame.php with updated structure and links

---
+++ icame/icame.php
@@ -0,0 +1,22 @@
+<?php
+include_once ("../captain_hook.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/static/css.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div class="ukraine-banner">#StandWithUkraine</div>
+<h1 class="center">I came list (wow much fancy)</h1>
+<a href="/submit/index.php">
+    <button class="center big-button">I Came list (write your name lol)</button>
+</a>
+<a href="/">
+    <button class="center little-big-button">Home</button>
+</a>
+
+</body>
+</html>
